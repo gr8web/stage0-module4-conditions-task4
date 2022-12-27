ca7828aea78aea9beacb7383a355a5351aca169b
Grigorij Ibe <2kx.datahell@gmail.com>
2022-12-27 08:07:50 +0100

    ternary operator

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        if (first > second && first > third) {
+            System.out.println(first);
+            return;
+        }
+        if (second > first && second > third) {
+            System.out.println(second);
+            return;
+        }
+        if (third > first && third > second) {
+            System.out.println(third);
+            return;
+        }
+        System.out.println(first);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int result = first > second ? first :
+            second > first ? second : first;
+
+        System.out.println(result);
     }
 }
